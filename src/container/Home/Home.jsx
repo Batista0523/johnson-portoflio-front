059f5f866974dc0cfc47f4450460e9beb48e55bb
elisaul batista <elisaulbatista@pursuit.org>
2023-11-26 16:17:44 -0500

    setting up components

---
+++ src/container/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Testimonial from '../Testomonial/Testimonial';
+const Home = () => {
+    return (
+        <div>
+            <h1>hello this is home</h1>
+            <Testimonial/>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/container/index.js
@@ -0,0 +1,5 @@
+export {default as Home} from './Home/Home.jsx';
+export {default as NewItem} from './Create/NewItem.jsx';
+export {default as ShowPage} from './Details/ShowPage.jsx';
+export {default as IndexPage} from './Index/IndexPage.jsx';
+export {default as Edit} from './Update/Edit.jsx';
